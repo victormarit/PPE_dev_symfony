de0d212c10b8dff0c743d16ed680fc920bd4c34b
victormarit <victormarit.95@gmail.com>
2020-11-22 19:11:33 +0100

    Proposition système de connexion

---
+++ ppeDEV/src/Controller/HomepageController.php
@@ -0,0 +1,20 @@
+<?php 
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class HomepageController extends AbstractController
+{
+
+
+
+    /**
+     * @Route("/homepage", name="homepage")
+     */
+    public function index()
+    {
+        return $this->render('homepage.html.twig');
+    }
+}
