4c11f24dc7fb00b1bb05c71f376138975a44edb5
victormarit <victormarit.95@gmail.com>
2020-11-30 21:56:34 +0100

    Création formulaire patientType

--- ppeDEV/src/Controller/HomepageController.php
+++ ppeDEV/src/Controller/HomepageController.php
@@ -2,7 +2,11 @@
 
 namespace App\Controller;
 
+use App\Entity\Patient;
+use App\Form\PatientType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class HomepageController extends AbstractController
@@ -11,10 +15,37 @@ class HomepageController extends AbstractController
 
 
     /**
-     * @Route("/homepage", name="homepage")
+     * @Route("/user/homepage", name="homepage")
      */
     public function index()
     {
         return $this->render('pages/homepage.html.twig');
+    }
+
+
+    /**
+     * @Route("/user/ajouterPatient", name="createPatient")
+     * @param Request $request
+     * @return Response
+     */
+    public function createNewStaffMember(Request $request):Response 
+    {
+        $user = new Patient;
+        dump('10');
+        $form =  $this->createForm(PatientType::class, $user);
+        dump('20');
+        $form->handleRequest($request);
+        
+        //if($form->isSubmitted()&& $form->isValid()){
+        //    $em = $this->getDoctrine()->getManager();
+        //    $em->persist($user);
+        //    $em->flush();
+        //    return $this->redirectToRoute('homepage'); 
+        //}
+        
+        
+        return $this->render('pages/addPatient.html.twig', [
+            "form" => $form->createView()
+        ]);
     }
 }
---
+++ ppeDEV/src/Form/PatientType.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Patient;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class PatientType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('lastName', TextType::class, [
+                "label" => "Nom",
+                "attr" => [
+                    "class" => "form-control"
+                ]
+            ])
+            ->add('firstName', TextType::class, [
+                "label" => "Prénom ",
+                "attr" => [
+                    "class" => "form-control"
+                ]
+            ])
+            ->add('socialSecurityNumber', TextType::class, [
+                "label" => "Numéro de sécurité social",
+                "attr" => [
+                    "class" => "form-control"
+                ]
+            ])
+            ->add('birthDate', DateType::class, [
+                "label" => "Date de naissance",
+                "attr" => [
+                    "class" => "form-control"
+                ]
+            ])
+            ->add('bloodType', ChoiceType::class, [
+                "label" => "Groupe Sanguin",
+                "required" => false,
+                "attr" => [
+                    "class" => "form-control"
+                ],
+                'placeholder' => 'Sélectionner un groupe sanguin',
+                'choices' => [
+                    'O' => 'O',
+                    'A' => 'A',
+                    'B' => 'B',
+                    'AB' => 'AB'
+                ]
+            ])
+            ->add("Ajouter", SubmitType::class)
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Patient::class,
+        ]);
+    }
+}
