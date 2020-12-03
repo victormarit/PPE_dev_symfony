3d0b5cc2475f3cf33e440c8807395c524b874b49
victormarit <victormarit.95@gmail.com>
2020-12-03 12:23:18 +0100

    Adding tests folder

---
+++ ppeDEV/tests/AppTest.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\tests;
+
+use PHPUnit\Framework\TestCase;
+
+class AppTests extends TestCase
+{
+    public function testAreWorking(){
+        $this->assertEquals(2, 1+1);
+    }
+} 
