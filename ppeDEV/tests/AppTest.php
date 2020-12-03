ac8fc2b0daf140b6f03340a129b7e9b97a4c699d
victormarit <victormarit.95@gmail.com>
2020-12-03 14:01:02 +0100

    Premier Test

--- ppeDEV/tests/AppTest.php
+++ ppeDEV/tests/AppTest.php
@@ -3,10 +3,21 @@
 namespace App\tests;
 
 use PHPUnit\Framework\TestCase;
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase; // permet de tester les controller et de voir les réponses
+use Symfony\Component\BrowserKit\Response;
 
-class AppTests extends TestCase
+class AppTests extends TestCase 
 {
     public function testAreWorking(){
         $this->assertEquals(2, 1+1);
+    }
+} 
+
+class ConnectionTests extends WebTestCase 
+{
+    public function testGetConnectionPage(){
+        $client = static::createClient();
+        $crawler = $client->request('GET', '/');
+        $this->assertResponseStatusCodeSame(200);
     }
 } 
