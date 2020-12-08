f3b8890126a18f673798be4e5c8d7e953ad71be2
jadDAOUK <jad190201.daouk@gmail.com>
2020-12-08 23:39:34 +0100

    continuation du front-end

--- ppeDEV/src/Form/PatientType.php
+++ ppeDEV/src/Form/PatientType.php
@@ -17,32 +17,47 @@ class PatientType extends AbstractType
     {
         $builder
             ->add('lastName', TextType::class, [
-                "label" => "Nom",
+                "label" => "Nom :",
+                "label_attr" => [
+                  "class" => "h3 ml-4 mb-0"
+                ],
                 "attr" => [
                     "class" => "form-control"
                 ]
             ])
             ->add('firstName', TextType::class, [
-                "label" => "Prénom ",
+                "label" => "Prénom :",
+                "label_attr" => [
+                    "class" => "h3 ml-4 mb-0"
+                ],
                 "attr" => [
                     "class" => "form-control"
                 ]
             ])
             ->add('socialSecurityNumber', TextType::class, [
-                "label" => "Numéro de sécurité social",
+                "label" => "Numéro de sécurité social :",
+                "label_attr" => [
+                    "class" => "h3 ml-4 mb-0"
+                ],
                 "attr" => [
                     "class" => "form-control"
                 ]
             ])
             ->add('birthDate', DateType::class, [
-                "label" => "Date de naissance",
+                "label" => "Date de naissance :",
+                "label_attr" => [
+                    "class" => "h3 ml-4 mb-0"
+                ],
                 "attr" => [
                     "class" => "form-control"
                 ],
                 "widget" => "single_text"
             ])
             ->add('bloodType', ChoiceType::class, [
-                "label" => "Groupe Sanguin",
+                "label" => "Groupe Sanguin :",
+                "label_attr" => [
+                    "class" => "h3 ml-4 mb-0"
+                ],
                 "required" => false,
                 "attr" => [
                     "class" => "form-control"
@@ -57,7 +72,7 @@ class PatientType extends AbstractType
             ])
             ->add("Sauvegarder", SubmitType::class, [
                 "attr" => [
-                    "class" => "btn btn-primary mt-3 w-100"
+                    "class" => "btn btn-primary w-100"
                 ]
             ])
         ;
