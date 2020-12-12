3676f24d17f365b4cc1046d3fc5a5e74ba31ad2c
victormarit <victormarit.95@gmail.com>
2020-12-12 19:51:11 +0100

    affichage de séjours

--- ppeDEV/src/Repository/StayRepository.php
+++ ppeDEV/src/Repository/StayRepository.php
@@ -56,6 +56,7 @@ class StayRepository extends ServiceEntityRepository
         Select service.name service, TIMEDIFF(stay.leave_date, stay.entry_date) duree, bed.number bed, hospital_room.number room, stay.entry_date entryDate, stay.leave_date leaveDate, stay.creation_date creationDate
         From stay, bed, hospital_room, service 
         Where id_Patient_id = :id and bed.id = stay.id_bed_id and bed.id_hospital_room_id = hospital_room.id and service.id = hospital_room.id_service_id
+        GROUP BY stay.id
         ORDER BY service.id DESC;
         ';
         $stmt = $conn->prepare($sql);
@@ -68,13 +69,15 @@ class StayRepository extends ServiceEntityRepository
     {
         $conn = $this->getEntityManager()->getConnection();
         $sql = '
-        SELECT patient.first_name firstname, patient.last_name lastname, stay.entry_date entryDate, stay.leave_date leaveDate, stay.creation_date creationDate, bed.number bedNumber, hospital_room.number roomNumber, service.name serviceName
+        SELECT patient.first_name firstname, TIMEDIFF(stay.leave_date, stay.entry_date) duration, patient.last_name lastname, stay.entry_date entryDate, stay.leave_date leaveDate, stay.creation_date creationDate, bed.number bedNumber, hospital_room.number roomNumber, service.name serviceName
         from stay, patient, bed, hospital_room, service
         where stay.id_patient_id = patient.id
         and stay.id_bed_id = bed.id
         and bed.id_hospital_room_id = hospital_room.id
         and hospital_room.id_service_id = service.id
+        GROUP BY stay.id
         ORDER BY stay.creation_date DESC
+        
         ';
         $stmt = $conn->prepare($sql);
         $stmt->execute();
@@ -87,13 +90,14 @@ class StayRepository extends ServiceEntityRepository
         $conn = $this->getEntityManager()->getConnection();
         $search = "%".$var."%";
         $sql = '
-        SELECT patient.first_name firstname, patient.last_name lastname, stay.entry_date entryDate, stay.leave_date leaveDate, stay.creation_date creationDate, bed.number bedNumber, hospital_room.number roomNumber, service.name serviceName
+        SELECT patient.first_name firstname, TIMEDIFF(stay.leave_date, stay.entry_date) duration, patient.last_name lastname, stay.entry_date entryDate, stay.leave_date leaveDate, stay.creation_date creationDate, bed.number bedNumber, hospital_room.number roomNumber, service.name serviceName
         from stay, patient, bed, hospital_room, service
         where stay.id_patient_id = patient.id
         and stay.id_bed_id = bed.id
         and (patient.last_name LIKE :val or patient.first_name LIKE :val)
         and bed.id_hospital_room_id = hospital_room.id
         and hospital_room.id_service_id = service.id
+        GROUP BY stay.id
         ORDER BY stay.creation_date DESC
         ';
         $stmt = $conn->prepare($sql);
