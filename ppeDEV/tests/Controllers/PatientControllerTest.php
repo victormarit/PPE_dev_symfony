6ed722aef527949164c612a052d3496ea543302d
victormarit <victormarit.95@gmail.com>
2020-12-13 12:02:47 +0100

    Ajout des nouveaux tests nouveau séjour patient

--- ppeDEV/tests/Controllers/PatientControllerTest.php
+++ ppeDEV/tests/Controllers/PatientControllerTest.php
@@ -74,4 +74,21 @@ class PatientControllerTest extends WebTestCase
         $client->request('GET', '/user/séjoursPatient/1/lastname/firstname');
         $this->assertResponseStatusCodeSame(302);
     }
+
+    //Test fonctionnel Route /user/nouveauSéjour
+    public function testNewStayRole_User(){
+        $client = SecurityControllerTest::getUserClient();
+        $client->request('GET', '/user/nouveauSéjour/1/{lastname}/{firstname}');
+        $this->assertResponseStatusCodeSame(200);
+    }
+    public function testNewStayRole_Admin(){
+        $client = SecurityControllerTest::getAdminClient();
+        $client->request('GET', '/user/nouveauSéjour/1/{lastname}/{firstname}');
+        $this->assertResponseStatusCodeSame(200);
+    }
+    public function testNewStayWithoutConnection(){
+        $client = static::createClient();
+        $client->request('GET', '/user/nouveauSéjour/1/{lastname}/{firstname}');
+        $this->assertResponseStatusCodeSame(302);
+    }
 }
